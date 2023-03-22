65ba2de46c288d27a8a638da34f7e5607c334dc7
JustoMateos21 <98715129+JustoMateos21@users.noreply.github.com>
2023-03-22 18:24:00 -0300

    connectamos-MongoDb-cap_4

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
 
 dotenv.config();
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.get("/", (req, res) => {
   res.send("funcionando");
 });
